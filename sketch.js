a64d828cb0def2e457cad181a549b7b8cc24c7ba
JJarrett01 <jarrettjemoy@gmail.com>
2023-10-19 08:53:29 -0500

    Changed to svg, added space variable

--- sketch.js
+++ sketch.js
@@ -2,13 +2,14 @@ window.open("", "_self",'resizable=no, scrollbars=no, width=200, height=320');
 var cwidth = window.innerWidth;
 var cheight = window.innerHeight*0.75;
 function setup(){
-  createCanvas(cwidth,cheight);
+  createCanvas(cwidth,cheight, SVG);
   noFill();
-  strokeWeight(1.5);
+  strokeWeight(1);
 }
 
 var maxNum = 1000;
 var minNum = 80;
+var space = 50;
 var mindim = Math.min(cwidth, cheight);
 var maxLevel = Math.round(Math.random()*(maxNum-minNum))+minNum;
 var eheight = mindim*0.27;
@@ -39,9 +40,9 @@ function draw(){
   if(!drawn){
   for(var level = 0; level < maxLevel; level++){
   if(!el){
-  rect(50, 50, ewidth, eheight);
+  rect(space, space, ewidth, eheight);
   }else{
-    ellipse(50, 50, ewidth, eheight);
+    ellipse(space +(ewidth/2), space+(eheight/2), ewidth, eheight);
   }
   rotate(100*Math.PI/maxLevel);
   var x = Math.exp(Math.log(0.01)/maxLevel)
@@ -62,9 +63,11 @@ hamburgerIcon.addEventListener('click', () => {
 document.getElementById('current-variable1').textContent = parseInt(ewidth);
 document.getElementById('current-variable2').textContent = parseInt(eheight);
 document.getElementById('current-variable3').textContent = parseInt(maxLevel);
+document.getElementById('current-variable4').textContent = parseInt(space);
 document.getElementById('variable1').value = parseInt(ewidth);
 document.getElementById('variable2').value =  parseInt(eheight);
 document.getElementById('variable3').value = parseInt(maxLevel);
+document.getElementById('variable4').value = parseInt(space);
 
 document.addEventListener('click', (e) => {
   if (!menu.contains(e.target) && e.target !== hamburgerIcon) {
@@ -77,10 +80,12 @@ function updateVariables() {
   ewidth= parseInt(document.getElementById('variable1').value);
   eheight = parseInt(document.getElementById('variable2').value);
   maxLevel = parseInt(document.getElementById('variable3').value);
+  space = parseInt(document.getElementById('variable4').value);
  drawn = false;
  document.getElementById('current-variable1').textContent = parseInt(ewidth);
   document.getElementById('current-variable2').textContent = parseInt(eheight);
   document.getElementById('current-variable3').textContent = parseInt(maxLevel);
+  document.getElementById('current-variable4').textContent = parseInt(space);
   el = document.getElementById('round-corners').checked;
 
 }
@@ -88,9 +93,11 @@ function updateMenu(){
 document.getElementById('current-variable1').textContent = parseInt(ewidth);
 document.getElementById('current-variable2').textContent = parseInt(eheight);
 document.getElementById('current-variable3').textContent = parseInt(maxLevel);
+document.getElementById('current-variable4').textContent = parseInt(space);
 document.getElementById('variable1').value = parseInt(ewidth);
 document.getElementById('variable2').value =  parseInt(eheight);
 document.getElementById('variable3').value = parseInt(maxLevel);
+document.getElementById('variable4').value = parseInt(space);
 document.getElementById('round-corners').checked = el;
 
 }
